0ff09234c5d7d0a810d31bca41eb19a9c7a8b2f4
Yichao Zhou <yichao.zhou@apple.com>
2024-06-30 16:24:43 -0400

    update structures

--- spinup/examples/1_simple_pg.py
+++ spinup/examples/1_simple_pg.py
@@ -4,31 +4,41 @@ from torch.distributions.categorical import Categorical
 from torch.optim import Adam
 import numpy as np
 import gymnasium as gym
-from gym.spaces import Discrete, Box
+from gymnasium.spaces import Discrete, Box
+
 
 def mlp(sizes, activation=nn.Tanh, output_activation=nn.Identity):
-    # Build a feedforward neural network.
     layers = []
-    for j in range(len(sizes)-1):
-        act = activation if j < len(sizes)-2 else output_activation
-        layers += [nn.Linear(sizes[j], sizes[j+1]), act()]
+    for j in range(len(sizes) - 1):
+        act = activation if j < len(sizes) - 2 else output_activation
+        layers += [nn.Linear(sizes[j], sizes[j + 1]), act()]
     return nn.Sequential(*layers)
 
-def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2, 
-          epochs=50, batch_size=5000, render=False):
 
+def train(
+    env_name="CartPole-v1",
+    hidden_sizes=[32],
+    lr=1e-3,
+    epochs=200,
+    batch_size=5000,
+    render=False,
+):
     # make environment, check spaces, get obs / act dims
     env = gym.make(env_name)
-    assert isinstance(env.observation_space, Box), \
-        "This example only works for envs with continuous state spaces."
-    assert isinstance(env.action_space, Discrete), \
-        "This example only works for envs with discrete action spaces."
+    env = env.unwrapped
+
+    assert isinstance(
+        env.observation_space, Box
+    ), "This example only works for envs with continuous state spaces."
+    assert isinstance(
+        env.action_space, Discrete
+    ), "This example only works for envs with discrete action spaces."
 
     obs_dim = env.observation_space.shape[0]
     n_acts = env.action_space.n
 
     # make core of policy network
-    logits_net = mlp(sizes=[obs_dim]+hidden_sizes+[n_acts])
+    logits_net = mlp(sizes=[obs_dim] + hidden_sizes + [n_acts])
 
     # make function to compute action distribution
     def get_policy(obs):
@@ -50,41 +60,45 @@ def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2,
     # for training policy
     def train_one_epoch():
         # make some empty lists for logging.
-        batch_obs = []          # for observations
-        batch_acts = []         # for actions
-        batch_weights = []      # for R(tau) weighting in policy gradient
-        batch_rets = []         # for measuring episode returns
-        batch_lens = []         # for measuring episode lengths
+        batch_obs = []  # for observations
+        batch_acts = []  # for actions
+        batch_weights = []  # for R(tau) weighting in policy gradient
+        batch_rets = []  # for measuring episode returns
+        batch_lens = []  # for measuring episode lengths
 
         # reset episode-specific variables
-        obs = env.reset()       # first obs comes from starting distribution
-        done = False            # signal from environment that episode is over
-        ep_rews = []            # list for rewards accrued throughout ep
+        observation, _ = env.reset()  # first obs comes from starting distribution
+        done = False  # signal from environment that episode is over
+        ep_rewards = []  # list for rewards accrued throughout ep
 
         # render first episode of each epoch
         finished_rendering_this_epoch = False
 
         # collect experience by acting in the environment with current policy
         while True:
-
             # rendering
             if (not finished_rendering_this_epoch) and render:
+                # In gymnasium, render() were executed during step(), which is too slow.
+                # Workaround it by turning off render_mode temporarily.
+                env.render_mode = "human"
                 env.render()
+                env.render_mode = ""
 
             # save obs
-            batch_obs.append(obs.copy())
+            batch_obs.append(observation.copy())
 
             # act in the environment
-            act = get_action(torch.as_tensor(obs, dtype=torch.float32))
-            obs, rew, done, _ = env.step(act)
+            action = get_action(torch.as_tensor(observation, dtype=torch.float32))
+            observation, reward, terminated, truncated, _ = env.step(action)
+            done = terminated or truncated
 
             # save action, reward
-            batch_acts.append(act)
-            ep_rews.append(rew)
+            batch_acts.append(action)
+            ep_rewards.append(reward)
 
             if done:
                 # if episode is over, record info about episode
-                ep_ret, ep_len = sum(ep_rews), len(ep_rews)
+                ep_ret, ep_len = sum(ep_rewards), len(ep_rewards)
                 batch_rets.append(ep_ret)
                 batch_lens.append(ep_len)
 
@@ -92,7 +106,7 @@ def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2,
                 batch_weights += [ep_ret] * ep_len
 
                 # reset episode-specific variables
-                obs, done, ep_rews = env.reset(), False, []
+                (observation, _), done, ep_rewards = env.reset(), False, []
 
                 # won't render again this epoch
                 finished_rendering_this_epoch = True
@@ -103,10 +117,11 @@ def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2,
 
         # take a single policy gradient update step
         optimizer.zero_grad()
-        batch_loss = compute_loss(obs=torch.as_tensor(batch_obs, dtype=torch.float32),
-                                  act=torch.as_tensor(batch_acts, dtype=torch.int32),
-                                  weights=torch.as_tensor(batch_weights, dtype=torch.float32)
-                                  )
+        batch_loss = compute_loss(
+            obs=torch.as_tensor(batch_obs, dtype=torch.float32),
+            act=torch.as_tensor(batch_acts, dtype=torch.int32),
+            weights=torch.as_tensor(batch_weights, dtype=torch.float32),
+        )
         batch_loss.backward()
         optimizer.step()
         return batch_loss, batch_rets, batch_lens
@@ -114,15 +129,19 @@ def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2,
     # training loop
     for i in range(epochs):
         batch_loss, batch_rets, batch_lens = train_one_epoch()
-        print('epoch: %3d \t loss: %.3f \t return: %.3f \t ep_len: %.3f'%
-                (i, batch_loss, np.mean(batch_rets), np.mean(batch_lens)))
+        print(
+            "epoch: %3d \t loss: %.3f \t return: %.3f \t ep_len: %.3f"
+            % (i, batch_loss, np.mean(batch_rets), np.mean(batch_lens))
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env_name', '--env', type=str, default='CartPole-v0')
-    parser.add_argument('--render', action='store_true')
-    parser.add_argument('--lr', type=float, default=1e-2)
+    parser.add_argument("--env_name", "--env", type=str, default="CartPole-v1")
+    parser.add_argument("--render", action="store_true")
+    parser.add_argument("--lr", type=float, default=1e-2)
     args = parser.parse_args()
-    print('\nUsing simplest formulation of policy gradient.\n')
+    print("\nUsing simplest formulation of policy gradient.\n")
     train(env_name=args.env_name, render=args.render, lr=args.lr)
